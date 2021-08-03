0c6131e303a17dfecf6ed1c348991983719cfa11
zakiwo <hiroki.ozaki39@gmail.com>
2021-08-03 04:59:53 +0000

    add evaluation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery.raty.js
 
 //= require rails-ujs
 //= require activestorage
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_070744) do
+ActiveRecord::Schema.define(version: 2021_08_03_023350) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_070744) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "favorites", force: :cascade do |t|
